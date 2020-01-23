d08d5d864dae9f1849c933fc97af7ace164cc390
artlemus <59708812+artlemus@users.noreply.github.com>
2020-01-22 23:39:19 -0800

    Add files via upload

---
+++ Excercise2.java
@@ -0,0 +1,23 @@
+public class Excercise2{
+
+public static void main(String args[]) {
+
+    String name="Arturo Lemus Jr";
+
+    int num1= 28;
+    int num2= 12;
+    int num3= 3000;
+
+    float result;
+    result = num2*num3;
+
+
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + num1);
+    System.out.println("Salary: " + result);
+
+
+
+
+  }
+}
